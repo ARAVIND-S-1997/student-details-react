90d7db735af452699604468e0926ec552793eef2
ARAVIND-S-1997 <itsmearavind4@gmail.com>
2022-04-02 21:58:46 +0530

    login completed

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -1,24 +1,56 @@
+// react bootstrap imports
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { useFormik } from 'formik';
+import Card from 'react-bootstrap/Card';
+
+// other packages imports
 import * as yup from 'yup';
 import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card'
+import axios from 'axios';
 
+// other file imports
+import { apiurl } from '../apiLink';
 
+// hooks imports
+import { useHistory } from 'react-router-dom';
+import { useFormik } from 'formik';
+
+// login validation schema
 const formValidation = yup.object({
     emailid: yup.string().required("Email field should not be empty"),
     password: yup.string().required("Password field should not be empty")
 })
+
+// login component
 export function Login() {
 
+    // formik functionality
     const { values, errors, touched, handleChange, handleSubmit, handleBlur } = useFormik({
         initialValues: { emailid: "", password: "" },
         validationSchema: formValidation,
-        onSubmit: (data) => console.log(data)
-    })
+        onSubmit: (data) => login(data)
+    });
 
+    const history = useHistory();
 
+    // login request
+    const login = (value) => {
+        axios({ url: `${apiurl}/login`, method: "POST", data: value })
+            .then((response) => {
+                if (response.status === 200) {
+                    try {
+                        const token = response.data.finalToken;
+                        const emailid = response.data.emailid;
+                        localStorage.setItem("token", token);
+                        localStorage.setItem("emailid", emailid);
+                        history.push("/welcomedashboard");
+                    } catch (errors) {
+                        console.log("Error is:", errors)
+                    }
+
+                }
+            })
+    }
 
 
     return (
@@ -27,8 +59,9 @@ export function Login() {
                 <Card.Body>
                     <Form onSubmit={handleSubmit} className="login-form">
                         <Form.Group className="login-emailid-part" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
 
+                            {/* Email field */}
+                            <Form.Label>Email address</Form.Label>
                             <input
                                 name="emailid"
                                 onChange={handleChange}
@@ -44,8 +77,9 @@ export function Login() {
                         </Form.Group>
 
                         <Form.Group className="login-password-part" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
 
+                            {/* password field */}
+                            <Form.Label>Password</Form.Label>
                             <input
                                 name="password"
                                 onChange={handleChange}
@@ -56,10 +90,12 @@ export function Login() {
                             />
                             {errors.password && touched.password ? (<div>{errors.password}</div>) : null}
                         </Form.Group>
+
+                        {/* links for forget password and signup */}
                         <Link>Forget password</Link>
                         <Link>Signup</Link>
-                        <Button className="login-form-button" variant="primary" type="submit">
 
+                        <Button className="login-form-button" variant="primary" type="submit">
                             Submit
                         </Button>
 

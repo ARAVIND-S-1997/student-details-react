c4fe5f9c833cc8a469b31f5ed7fcbf3b67e3a166
ARAVIND-S-1997 <itsmearavind4@gmail.com>
2022-05-06 12:04:00 +0530

    made some changes in auth imports

--- src/App.js
+++ src/App.js
@@ -19,62 +19,135 @@ import { Addmarks } from "./components/addMarksComponent";
 import { Userdetails } from "./components/userdetailsComponent";
 import { Edituserdetails } from "./components/edituserdetailsComponent";
 import { Editmarks } from "./components/editmarksComponent";
+import { Redirect } from "react-router-dom";
+
+
 // Root component
 function App() {
 
+  const authtoken = localStorage.getItem("token");
+
+
   return (
     <>
-      <NavBar />
-      <Switch>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-        <Route exact path="/forgetpassword">
-          <Forgetpassword />
-        </Route>
-        <Route exact path="/changepassword/:value">
-          <Changepassword />
-        </Route>
-        <Route exact path="/welcomedashboard">
-          <Welcomedashboard />
-        </Route>
-        <Route exact path="/myclassroom">
-          <Myclassroom />
-        </Route>
-        <Route exact path="/createclassroom">
-          <Createclassroom />
-        </Route>
-        <Route exact path="/classdetails/:id">
-          <Classdetails />
-        </Route>
-        <Route exact path="/addstudent/:id">
-          <Addstudent />
-        </Route>
-        <Route exact path="/studentinfo/:id">
-          <IndividualStudentDetails />
-        </Route>
-        <Route exact path="/editstudent/:id">
-          <EditStudentInfo />
-        </Route>
-        <Route exact path="/addmarks/:id">
-          <Addmarks />
-        </Route>
-        <Route exact path="/userdetails">
-          <Userdetails />
-        </Route>
-        <Route exact path="/edituserdetails/:id">
-          <Edituserdetails />
-        </Route>
-        <Route exact path="/editmarks/:id">
-          <Editmarks />
-        </Route>
-      </Switch>
-    </>
-
+    <NavBar />
+    <Switch>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/forgetpassword">
+        <Forgetpassword />
+      </Route>
+      <Route exact path="/changepassword/:value">
+        <Changepassword />
+      </Route>
+      <Route exact path="/welcomedashboard">
+        <Welcomedashboard />
+      </Route>
+      <Route exact path="/myclassroom">
+        <Myclassroom />
+      </Route>
+      <Route exact path="/createclassroom">
+        <Createclassroom />
+      </Route>
+      <Route exact path="/classdetails/:id">
+        <Classdetails />
+      </Route>
+      <Route exact path="/addstudent/:id">
+        <Addstudent />
+      </Route>
+      <Route exact path="/studentinfo/:id">
+        <IndividualStudentDetails />
+      </Route>
+      <Route exact path="/editstudent/:id">
+        <EditStudentInfo />
+      </Route>
+      <Route exact path="/addmarks/:id">
+        <Addmarks />
+      </Route>
+      <Route exact path="/userdetails">
+        <Userdetails />
+      </Route>
+      <Route exact path="/edituserdetails/:id">
+        <Edituserdetails />
+      </Route>
+      <Route exact path="/editmarks/:id">
+        <Editmarks />
+      </Route>
+    </Switch>
+  </>
   );
 }
 
 export default App;
+
+
+{/* <>
+<NavBar />
+<Switch>
+
+  <Route exact path="/login">
+    <Login />
+  </Route>
+
+  <Route exact path="/signup">
+    <Signup />
+  </Route>
+
+  <Route exact path="/forgetpassword">
+    <Forgetpassword />
+  </Route>
+
+  <Route exact path="/changepassword/:value">
+    <Changepassword />
+  </Route>
+
+  <Route exact path="/welcomedashboard">
+   <Welcomedashboard /> 
+  </Route>
+
+  <Route exact path="/myclassroom">
+    {(authtoken !== null) ? <Myclassroom /> : <Redirect to="/login" />}
+  </Route>
+
+  <Route exact path="/createclassroom">
+    {(authtoken !== null) ? <Createclassroom /> : <Redirect to="/login" />}
+  </Route>
+
+  <Route exact path="/classdetails/:id">
+    {(authtoken !== null) ? <Classdetails /> : <Redirect to="/login" />}
+  </Route>
+
+  <Route exact path="/addstudent/:id">
+    {(authtoken !== null) ? <Addstudent /> : <Redirect to="/login" />}
+  </Route>
+
+  <Route exact path="/studentinfo/:id">
+    {(authtoken !== null) ? <IndividualStudentDetails /> : <Redirect to="/login" />}
+  </Route>
+
+  <Route exact path="/editstudent/:id">
+    {(authtoken !== null) ? <EditStudentInfo /> : <Redirect to="/login" />}
+  </Route>
+
+  <Route exact path="/addmarks/:id">
+    {(authtoken !== null) ? <Addmarks /> : <Redirect to="/login" />}
+  </Route>
+
+  <Route exact path="/userdetails">
+    {(authtoken !== null) ? <Userdetails /> : <Redirect to="/login" />}
+
+  </Route>
+  <Route exact path="/edituserdetails/:id">
+    {(authtoken !== null) ? <Edituserdetails /> : <Redirect to="/login" />}
+  </Route>
+
+  <Route exact path="/editmarks/:id">
+    {(authtoken !== null) ? <Editmarks /> : <Redirect to="/login" />}
+  </Route>
+
+</Switch>
+</> */}
